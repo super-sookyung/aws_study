2c6828f45c81872234fe78c40224a081d0757489
mrkwon <erased11@gmail.com>
2019-04-10 15:15:26 +0900

    frontend example initialize

---
+++ front/src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" placeholder="please input text">
+    <button onclick="clickPostRequestBtn">Post Request</button>
+  </div>
+</template>
+
+<script>
+var card = { suit: "하트", rank: "A" }
+
+export default {
+  methods: {
+    clickPostRequestBtn() {
+      console.log("btn clicked")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
